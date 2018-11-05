660aa177501b1ad12d7c09c4627d4a3987a5ec98
LoanaGentric <32701819+LoanaGentric@users.noreply.github.com>
2018-11-05 15:53:24 +0100

    Init

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const shooter = {}
 
 /**
- *  Set up
+ * Set up
  */
-
 shooter.$container = document.querySelector('.shooter')
 shooter.$targets = shooter.$container.querySelector('.targets')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$score = shooter.$container.querySelector('.score .value')
+shooter.score = 0
 
 /**
- * Method
+ * Methods
  */
-
-shooter.addTarget = () => {
+shooter.addTarget = () =>
+{
+    // Create element
     const $target = document.createElement('div')
     $target.classList.add('target')
+    $target.style.top = `${Math.random() * 100}%`
+    $target.style.left = `${Math.random() * 100}%`
     shooter.$targets.appendChild($target)
+
+    // Listen to mouse enter
+    $target.addEventListener('mouseenter', () =>
+    {
+        shooter.shootTarget($target)
+    })
 }
 
-shooter.addTarget()
+shooter.shootTarget = (_$target) =>
+{
+    _$target.remove()
+    shooter.addTarget()
+    shooter.score = shooter.score + 3
+    shooter.$score.textContent = shooter.score
+}
+
+shooter.addTarget() 
